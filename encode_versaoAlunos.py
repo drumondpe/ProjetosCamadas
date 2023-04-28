1c581af3bf8f774b59c9eaf75d14ca061c14f240
drumondpe <pedrogsd@al.insper.edu.br>
2023-04-28 14:42:13 -0300

    dicionário de frequências

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -6,9 +6,9 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 
 #funções a serem utilizadas
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
+# def signal_handler(signal, frame):
+#         print('You pressed Ctrl+C!')
+#         sys.exit(0)
 
 #converte intensidade em Db, caso queiram ...
 def todB(s):
@@ -23,6 +23,9 @@ def main():
    
     #********************************************instruções*********************************************** 
     # seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada
+
+
+
     # então inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF
     # agora, voce tem que gerar, por alguns segundos, suficiente para a outra aplicação gravar o audio, duas senoides com as frequencias corresposndentes à tecla pressionada, segundo a tabela DTMF
     # Essas senoides tem que ter taxa de amostragem de 44100 amostras por segundo, entao voce tera que gerar uma lista de tempo correspondente a isso e entao gerar as senoides
@@ -50,3 +53,38 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def gera_senoides(caractere, time, fs):
+    if caractere == 1:
+        x1, y1 = generateSin(1209, time, fs)
+        x2, y2 = generateSin(697, time, fs)
+        signal = y1+y2
+        return signal
+        pass
+
+def generateSin(freq, time, fs):
+    n = time*fs #numero de pontos
+    x = np.linspace(0.0, time, n)  # eixo do tempo
+    s = np.sin(freq*x*2*np.pi)
+    plt.figure()
+    plt.plot(x,s)
+    return (x, s)
+
+frequencias = {
+    1: (1209, 697),
+    2: (1336, 697),
+    3: (1477, 697),
+    4: (1209, 770),
+    5: (1336, 770),
+    6: (1477, 770),
+    7: (1209, 852),
+    8: (1336, 852),
+    9: (1477, 852),
+    0: (1336, 941),
+    'X': (1209, 941),
+    '#': (1477, 941),
+    'A': (1633, 697),
+    'B': (1633, 770),
+    'C': (1633, 852),
+    'D': (1633, 941),
+}
